df4392f1f2d745197d48b70aeee50bd4e4a6c414
Taras <mythbuster.lviv@gmail.com>
2021-03-24 18:37:02 +0200

    Add ship unloading

--- src/com/company/tasks/PierLoader.java
+++ src/com/company/tasks/PierLoader.java
@@ -19,8 +19,15 @@ public class PierLoader implements Runnable {
     @Override
     public void run() {
         while (true) {
-            Thread.
+            Thread.currentThread().setName("Loader" + shipType);
 
+            try {
+                Thread.sleep(500);
+
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
